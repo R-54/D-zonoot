8b133334043d9b65517534e80e3189f11b66539c
IvanRodriguezSoria <vendeta.ivan@hotmail.com>
2016-10-03 23:34:57 -0500

    Diagonal movement added.

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,16 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create: create, update: update});
+var isLookingRight = false;
 var player;
-var lookingRight = false;
+var cursors;
 
 function preload()
 {
     // Background.
     game.load.image('cave', 'assets/cave.jpg');
-    // Character.
-    game.load.spritesheet('dude', 'assets/diver-swim.png', 73, 50);
+    // Character.swimming
+    game.load.spritesheet('diver', 'assets/diver-swim.png', 73, 50);
+    // Character attacking witha knive
+    game.load.spritesheet('diver-knive', 'assets/diver-knive.png', 80, 50);
 }
 
 function create()
@@ -20,54 +23,68 @@ function create()
 
     // Add a player sprite to the game, with physics, gravity
     // and bounds enabled.
-    player = game.add.sprite(32, game.world.height - 150, 'dude');
+    player = game.add.sprite(32, game.world.height - 150, 'diver');
     game.physics.arcade.enable(player);
     player.body.gravity.y = 10;
     player.anchor.setTo(0.5, 0.5);
     player.body.collideWorldBounds = true;
     player.animations.add('swim', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, true);
+
+    // Populate cursors.
+    cursors = game.input.keyboard.createCursorKeys();
 }
 
 function update()
 {
-    var cursors = game.input.keyboard.createCursorKeys();
     player.body.velocity.x = 0;
     
     // The user press the left button.
     if(cursors.left.isDown)
     {
-        if(lookingRight)
+        if(isLookingRight)
         {
             player.scale.x *= -1;
-            lookingRight = false;
+            isLookingRight = false;
         }
+        
+        // Check if the player wants to move in a diagonal direction.
+        if(cursors.up.isDown)
+            player.body.velocity.y = -100;
+        else if(cursors.down.isDown)
+            player.body.velocity.y = 100;
+
         player.body.velocity.x = -150;
         player.animations.play('swim');
     }
+    
     // The user press the right button.
     else if(cursors.right.isDown)
     {
-        if(!lookingRight)
+        if(!isLookingRight)
         {
             player.scale.x *= -1;
-            lookingRight = true;
+            isLookingRight = true;
         }
+
+        // Check if the player wants to move in a diagonal direction.
+        if(cursors.up.isDown)
+            player.body.velocity.y = -100;
+        else if(cursors.down.isDown)
+            player.body.velocity.y = 100;
+
         player.body.velocity.x = 150;
         player.animations.play('swim');
     }
+    
     // The user press the up button.
     else if(cursors.up.isDown)
-    {
-        player.body.velocity.y = -80;
-    }
+        player.body.velocity.y = -100;
+    
     // The user press the down button.
     else if(cursors.down.isDown)
-    {
-        player.body.velocity.y = 80;
-    }
+        player.body.velocity.y = 100;
+
     // The user stay still.
     else
-    {
         player.animations.play('swim');
-    }
 }
